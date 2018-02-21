16a5f17564afa5eb83c7f1fa21c124914a829dec
Unknown <19trant@elmbrookstudents.org>
2018-02-21 15:38:48 -0600

    Fields and methods

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,14 @@
 
 public class Person {
+	private String name;
+	private int salary;
 
+	public Person() {
+		name = "Bob";
+		salary = 30;
+	}
+
+	public void moreMoney() {
+		salary += 1000;
+	}
 }
