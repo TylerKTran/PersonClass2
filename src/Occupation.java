3c59a16ca2889b25f778100c680d5a7e6b2134a7
Ben Tan <bendantan@gmail.com>
2018-02-21 16:01:06 -0600

    Occupation

--- src/Occupation.java
+++ src/Occupation.java
@@ -1,4 +1,13 @@
 
 public class Occupation {
-
+	String CompanyName="";
+	String JobTitle="";
+	public Occupation(){
+		CompanyName="Backspace Inc.";
+		JobTitle="Scrumling";
+	}
+	public String nameTag() {
+		String tag=CompanyName+"/n"+JobTitle;
+		return tag;
+	}
 }
