4ff391aa05df3a31e8bae923ee312a81e6089621
mridulwaldia <mridulsinghwaldia@gmail.com>
2018-03-07 15:45:07 -0600

    Updated
    No more test

--- src/Schedule.java
+++ src/Schedule.java
@@ -5,16 +5,15 @@ public class Schedule {
 	
 	public Schedule() {
 		
-		scheduledMeetings[1] = "Meeting 1";
-		scheduledMeetings[2] = "Meeting 2";
-		scheduledMeetings[3] = "Meeting 3";
-		scheduledMeetings[4] = "Meeting 4";
-		scheduledMeetings[5] = "Meeting 5";
+		scheduledMeetings[0] = "Meeting 1";
+		scheduledMeetings[1] = "Meeting 2";
+		scheduledMeetings[2] = "Meeting 3";
+		scheduledMeetings[3] = "Meeting 4";
+		scheduledMeetings[4] = "Meeting 5";
 	
 	}
 	public String mySchedule() {
 		
-		return scheduledMeetings[1] + "/n" + scheduledMeetings[2] + "/n" + scheduledMeetings[3] + "/n" + scheduledMeetings[4] + "/n" + scheduledMeetings[5];
-		
+		return scheduledMeetings[0] + "/n" + scheduledMeetings[1] + "/n" + scheduledMeetings[2] + "/n" + scheduledMeetings[3] + "/n" + scheduledMeetings[4];
 	}
 }
--- src/test.java
+++
@@ -1,9 +0,0 @@
-
-public class test {
-
-	public static void main(String[] args) {
-		Person bob = new Person();
-
-	}
-
-}
